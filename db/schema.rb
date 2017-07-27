54733a17ef1af388ac82d0cb92032a19bade3bcc
Tyler Brown <tyler@sliceofnet.com>
2017-07-27 01:34:43 -0400

    setting up password and signing up

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20170721035918) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password_digest"
     t.integer  "rank"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
