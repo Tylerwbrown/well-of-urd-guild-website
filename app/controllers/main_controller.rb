4858ded341a8103ccd066665f1e6a2c9a9b104cb
Tyler Brown <tyler@sliceofnet.com>
2017-07-27 01:34:51 -0400

    setting up animations

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,12 @@
 class MainController < ApplicationController
 
   def home
-    @user = User.new
+    session[:current_user_id]
+    @user = if session[:current_user_id].present?
+      User.find(session[:current_user_id])
+    else
+      User.new
+    end
   end
 
 end
