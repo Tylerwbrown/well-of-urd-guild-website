e97ee3129da262f8e27ab01d33a2e72e304a314b
Tyler Brown <tyler@sliceofnet.com>
2017-07-28 01:37:09 -0400

    styled more consistently

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,11 @@
 class MainController < ApplicationController
-
   def home
     session[:current_user_id]
-    @user = if session[:current_user_id].present?
-      User.find(session[:current_user_id])
-    else
-      User.new
-    end
+    @user =
+      if session[:current_user_id].present?
+        User.find(session[:current_user_id])
+      else
+        User.new
+      end
   end
-
 end
